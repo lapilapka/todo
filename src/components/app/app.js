1d7a4b78fc715fb9c4c6e69433706399401bbdad
Katya Belousova <lapilapka@yahoo.com>
2021-03-27 13:55:48 +0300

    Added buttons important elements and delete elements

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import ItemStatusFilter from "../item-status-filter";
 import TodoList from "../todo-list";
 import "./app.css"
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1 },
-        {label: 'Build react App', important: true, id: 2 },
-        {label: 'Have a lunch', important: false, id: 3 }
-    ];
+constructor() {
+    super();
+    this.state = {
+        todoData: [
+    {label: 'Drink Coffee', important: false, id: 1 },
+    {label: 'Build react App', important: true, id: 2 },
+    {label: 'Have a lunch', important: false, id: 3 }
+    ]
+    };
+}
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+            const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    render () {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem }/>
             </div>
-            <TodoList todos={todoData}/>
-        </div>
-    );
+        );
+    };
 };
 
-export default App;
