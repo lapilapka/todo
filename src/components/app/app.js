15d0dd1e1387920bf83e86e1d40e25614d6c888e
Katya Belousova <lapilapka@yahoo.com>
2021-03-27 14:56:06 +0300

    Added button Add Item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,15 @@ import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import ItemStatusFilter from "../item-status-filter";
 import TodoList from "../todo-list";
+import ItemAddForm from "../item-add-form";
 import "./app.css"
 
 export default class App extends Component {
 
+    maxId = 100;
 constructor() {
     super();
+
     this.state = {
         todoData: [
     {label: 'Drink Coffee', important: false, id: 1 },
@@ -28,18 +31,37 @@ constructor() {
             };
         });
     };
+    addItem = (text) => {
+        const newItem  = {
+          label: text,
+          important: false,
+          id: this.maxId++
+        };
+        this.setState(({ todoData}) => {
+
+            const newArr = [
+                ...todoData, newItem
+            ]
+            return {
+                todoData: newArr
+            };
+        });
+    }
+    ;
 
     render () {
         return (
             <div className="todo-app">
                 <AppHeader toDo={1} done={3}/>
                 <div className="top-panel d-flex">
-                    <SearchPanel/>
-                    <ItemStatusFilter/>
+                    <SearchPanel />
+                    <ItemStatusFilter />
                 </div>
                 <TodoList
                     todos={this.state.todoData}
                     onDeleted={this.deleteItem }/>
+                <ItemAddForm
+                    onItemAdded={this.addItem}/>
             </div>
         );
     };
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -25,6 +25,7 @@ export default class TodoListItem extends Component {
                 };
             });
         };
+
     }
 
     render() {
